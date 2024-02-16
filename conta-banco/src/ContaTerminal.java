c9b4fde4f203a84814c787c510a11fcf4070542b
Jessé Henrique Pereira <henrique.jessex@gmail.com>
2024-02-16 20:35:45 -0300

    feat: Criancao do projeto

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,13 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        // TODO - importar scanner e criar objeto scanner
+
+        // TODO - exibir mensagens para o usuario
+
+        // TODO - ler o valor do deposito digitado pelo usuario
+
+        // TODO - exibir mensagem de conta criada
+
+    }
+}
